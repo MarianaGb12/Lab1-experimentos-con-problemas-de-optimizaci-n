10f48a2505b312b62ed6c2a8652689504f4b5a5d
MarianaGb12 <98919850+MarianaGb12@users.noreply.github.com>
2024-09-15 21:33:06 -0500

    .

--- VenvProject/Scripts/activate.bat
+++ VenvProject/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\Jonathan\Desktop\Lab01\VenvProject
+set VIRTUAL_ENV=C:\Users\maria\Desktop\SeptimoSemestre\Optimizacion\Laboratorio1\Lab1-experimentos-con-problemas-de-optimizaci-n\VenvProject
 
 if not defined PROMPT set PROMPT=$P$G
 
